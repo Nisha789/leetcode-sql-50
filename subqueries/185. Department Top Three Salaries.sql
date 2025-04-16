472b8593cf74c23001dcf696d774ad66d3d0a2bc
Nisha <46855901+Nisha789@users.noreply.github.com>
2025-04-16 07:26:08 +0530

    Create 185. Department Top Three Salaries.sql

---
+++ subqueries/185. Department Top Three Salaries.sql
@@ -0,0 +1,7 @@
+select Department,Employee,Salary from
+(select d.name as Department,e.name as Employee, e.salary as Salary,
+dense_rank() over(partition by d.name order by e.salary desc) as ranks
+from employee e
+left join department d
+on e.departmentId = d.id) temp
+where ranks <= 3
